c16d2c62ad89044ecede651c541e883e5302c246
camazog1 <CarlosMazoG>
2023-09-13 22:09:59 -0500

    base y cards

--- qtaproject/settings.py
+++ qtaproject/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'qtaproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'qtaproject/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'qtaproject/static'
+]
