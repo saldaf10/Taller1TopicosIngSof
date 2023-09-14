acaca6420e1485536a0eba87ce35e32cd102a8e1
jmmunozg1 <113315113+jmmunozg1@users.noreply.github.com>
2023-09-13 22:16:01 -0500

    Cambios en las vistas

--- qta/views.py
+++ qta/views.py
@@ -14,4 +14,21 @@ def more_info(request):
     return render(request, 'more_info.html')
 
 def ticket(request):
+    if request.method == 'POST':
+        ticket_number = request.POST.get('ticket_number')
+        call_time = request.POST.get('call_time')
+        priority = request.POST.get('priority')
+        discussion = request.POST.get('discussion')
+        state = request.POST.get('state')
+        source = request.POST.get('source')
+        equipment = request.POST.get('equipment')
+        contact_number = request.POST.get('contact_number')
+        contact_name = request.POST.get('contact_name')
+
+        # Crea el nuevo ticket en la base de datos
+        nuevo_ticket = Ticket(ticket_number=ticket_number, call_time=call_time, priority=priority, discussion=discussion, state=state, source=source, equipment=equipment, contact_number=contact_number, contact_name=contact_name )
+        nuevo_ticket.save()
+
+        return HttpResponse('Ticket creado exitosamente')  
+
     return render(request, 'ticket.html')
